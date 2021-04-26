c7a0907307f829d4ec4e3130dbf61ec8e150d37e
edmobe <e.d.mobe@hotmail.com>
2021-04-25 20:23:35 -0600

    Implement maximum LPYR height calculation.

--- Visual Studio Community 2019/VideoMagnification/VideoMagnification/Source.cpp
+++ Visual Studio Community 2019/VideoMagnification/VideoMagnification/Source.cpp
@@ -2,14 +2,42 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
+constexpr auto MAX_FILTER_SIZE = 5;
 
 using namespace cv;
 using namespace std;
+
+// Global variables
+int pyramidHeight;
+
+int maxPyrHt(int frameWidth, int frameHeight, int filterSizeX, int filterSizeY) {
+    // 1D image
+    if (frameWidth == 1 || frameHeight == 1) {
+        frameWidth = frameHeight = 1;
+        filterSizeX = filterSizeY = filterSizeX * filterSizeY;
+    }
+    // 2D image
+    else if (filterSizeX == 1 || filterSizeY == 1) {
+        filterSizeY = filterSizeX;
+    }
+    // Stop condition
+    if (frameWidth < filterSizeX || frameWidth < filterSizeY ||
+        frameHeight < filterSizeX || frameHeight < filterSizeY)
+    {
+        pyramidHeight = 0;
+    }
+    // Next level
+    else {
+        pyramidHeight = 1 + maxPyrHt(frameWidth / 2, frameHeight / 2, filterSizeX, filterSizeY);
+    }
+    return pyramidHeight;
+}
 
 int main() {
 
     // Create a VideoCapture object and open the input file
     // If the input is the web camera, pass 0 instead of the video file name
+    // VideoCapture video(0);
     VideoCapture video("vid/guitar.mp4");
 
     // Extract video info
@@ -18,6 +46,10 @@ int main() {
     int nChannels = 3;
     int fr = video.get(CAP_PROP_FPS);
     int len = video.get(CAP_PROP_FRAME_COUNT);
+    int max_ht = 1 + maxPyrHt(vidWidth, vidHeight, MAX_FILTER_SIZE, MAX_FILTER_SIZE);
+
+    cout << max_ht << endl;
+    return 0;
 
     // Check if video opened successfully
     if (!video.isOpened()) {
@@ -63,4 +95,3 @@ int main() {
 
     return 0;
 }
-
